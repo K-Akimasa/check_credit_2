d19decf811d957683607cc7d8f82c907688b64b7
K-Akimasa <kimura.akimasa@gmail.com>
2014-12-14 01:04:43 +0900

    fixed

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 $(function() {
-	var $test_chk1 = $('#test_chk1');
-	console.log($test_chk1);
-	var $test_chkbox1 = $test_chk1.find(':checkbox')
-	console.log($test_chkbox1);
-	$test_chk1.change(function(){
+	var total = 0;
+	var $test = $('.test');
+	console.dir($test);
+	console.dir($test.length);
+	$test.change(function(){
 		if ($(this).is(':checked')) {
 			console.log("on");
+			console.log("\t" + $(this).attr('name'));
+			console.log("\t" + $(this).attr('value'));
+			total += parseInt($(this).attr('value'));
 		} else {
 			console.log("off");
+			total -= parseInt($(this).attr('value'));
 		}
+		console.log("total : " + total);
 	});
+	
 })
