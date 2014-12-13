8ac41787d9b9545696e9c7627bf4c2a6e23f95a7
K-Akimasa <kimura.akimasa@gmail.com>
2014-12-14 00:32:07 +0900

    fixed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 $(function() {
-	console.log("Hello");
+	var $test_chk1 = $('#test_chk1');
+	console.log($test_chk1);
+	var $test_chkbox1 = $test_chk1.find(':checkbox')
+	console.log($test_chkbox1);
+	$test_chk1.change(function(){
+		if ($(this).is(':checked')) {
+			console.log("on");
+		} else {
+			console.log("off");
+		}
+	});
 })
