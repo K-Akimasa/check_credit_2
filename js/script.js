54c3efdce1faf62b1e12b3b1fe41a87fbaf8990e
K-Akimasa <kimura.akimasa@gmail.com>
2014-12-24 14:40:13 +0900

    fixed

--- js/script.js
+++ js/script.js
@@ -4,10 +4,13 @@ var credit_num = {
 	humanity_total : 0,					// 人文科目単位数
 	society_total : 0,					// 社会科学単位数
 	kiban_non_comp_sbjs : [0],			// 基盤教育 未履修科目
+	
 	cmn_pro_base_total : 0,				// 共通専門基礎科目単位数
 	cmn_pro_base_non_comp_sbjs : [0],	// 共通専門基礎 未履修科目
+	
 	pro_req_total : 0,					// 専門必修科目単位数
 	pro_req_non_comp_sbjs : [0],		// 専門必修科目 未履修科目
+	
 	pro_sel_A_total : 0,				// 専門選択A群単位数
 	pro_sel_A_non_comp_sbjs : [0],		// 専門選択A群 未履修科目
 	
@@ -53,64 +56,66 @@ $(function() {
 	/*============*/
 	var $result = $('#result');		
 	var $close_btn = $('#閉じる'); 
+	
 	var $result_list = $('#集計結果'); 
+	var $kiban_non_list = $('#基盤教育科目未履修');
+	var $cmn_pro_base_non_list = $('#共通専門基礎科目未履修');
+	var $pro_req_non_list = $('#専門必修科目未履修');
+	var $pro_sel_A_non_list = $('#専門選択A群未履修');
 	
-	
+	/* 各DOM */
+	var $kiban_chkbox = $('#基盤教育科目 :checkbox');
+	var $cmn_pro_base_chkbox = $('#共通専門基礎科目 :checkbox');
+	var $pro_req_chkbox = $('#専門必修科目 :checkbox');
+	var $pro_sel_A_chkbox = $('#専門選択A群 :checkbox');
+		
 	/*==================*/
 	/*== イベント設定 ==*/
 	/*==================*/
 	// ボタンが押されたら，ダイアログのメッセージをセットする
 	$result.click(function () {
-	
-		// 		
+		// 基盤教育基礎科目
 		$result_list.append(
 			'<li class="ui-first-child ui-li-static ui-body-inherit">'
 			 + "基盤教育基礎科目 : " + String(credit_num.kiban_total)
 			 + '</li>'
 		);
-		
-		// 
+		// 人文科目
 		$result_list.append(
 			'<li class="ui-li-static ui-body-inherit">'
 			 + " ― 人文科目 : " + String(credit_num.humanity_total)
 			 + '</li>'
 		);
-		
-		// 
+		// 社会科目
 		$result_list.append(
 			'<li class="ui-li-static ui-body-inherit">'
 			 + " ― 社会科目：" + String(credit_num.society_total)
 			 + '</li>'
 		);
-		
-		// 
+		// 共通専門基礎科目
 		$result_list.append(
 			'<li class="ui-li-static ui-body-inherit">'
 			 + "共通専門基礎科目：" + String(credit_num.cmn_pro_base_total)
 			 + '</li>'
 		);
-		
-		// 	
+		// 専門必修科目
 		$result_list.append(
 			'<li class="ui-li-static ui-body-inherit">'
 			 + "専門必修科目：" + String(credit_num.pro_req_total)
 			 + '</li>'
 		);
-		
-		// 
+		// 専門選択A群
 		$result_list.append(
 			'<li class="ui-li-static ui-body-inherit">'
 			 + "専門選択A群：" + String(credit_num.pro_sel_A_total)
 			 + '</li>'
 		);
-		
-		// 
+		// 合計
 		$result_list.append(
 			'<li class="ui-last-child ui-li-static ui-body-inherit">'
 			 + "合計：" + String(credit_num.getTotal())
 			 + '</li>'
 		);
-		
 		
 		// 基盤教育の未履修科目を取得する
 		/*
@@ -124,6 +129,49 @@ $(function() {
 		}
 		*/
 		
+		var kiban_non_length = credit_num.kiban_non_comp_sbjs.length;
+		console.log("基盤 : " + kiban_non_length);
+						
+		for (var i = 0; i < kiban_non_length; i++) {
+			if (credit_num.kiban_non_comp_sbjs[i]) {
+				$kiban_non_list.append(
+					'<li class="ui-li-static ui-body-inherit">'
+					+ credit_num.kiban_non_comp_sbjs[i]
+					+ '</li>'
+				);
+			}
+		}
+		
+		
+		var cmn_pro_base_non_length = credit_num.cmn_pro_base_non_comp_sbjs.length;
+		console.log("共通専門 : " + cmn_pro_base_non_length);
+		
+		for (var i = 0; i < kiban_non_length; i++) {
+			if (credit_num.cmn_pro_base_non_comp_sbjs[i]) {
+				$cmn_pro_base_non_list.append(
+					'<li class="ui-li-static ui-body-inherit">'
+					+ credit_num.cmn_pro_base_non_comp_sbjs[i]
+					+ '</li>'
+				);
+			}
+		}		
+		
+		var pro_req_non_length = credit_num.pro_req_non_comp_sbjs.length;
+		console.log("専門必修 : " + pro_req_non_length);
+		
+		for (var i = 0; i < kiban_non_length; i++) {
+			if (credit_num.pro_req_non_comp_sbjs[i]) {
+				$pro_req_non_list.append(
+					'<li class="ui-li-static ui-body-inherit">'
+					+ credit_num.pro_req_non_comp_sbjs[i]
+					+ '</li>'
+				);
+			}
+		}
+		
+		var pro_sel_A_non_length = credit_num.pro_sel_A_non_comp_sbjs.length;
+		console.log("専門選択A群 : " + pro_sel_A_non_length);
+		
 		// 基盤教育の未履修科目の設定
 		//$('#popup #kiban_non_sbjs p').html(non_sbjs_msg);
 	});
@@ -131,9 +179,13 @@ $(function() {
 	/* 閉じるボタンが押されたら，liを空にする */
 	$close_btn.click(function () {
 		$result_list.empty();
+		$kiban_non_list.empty();
+		$cmn_pro_base_non_list.empty();
 	});
 	
 });
+
+/* 各科目のイベント設定 */
 
 /*********************************
  * 基盤教育科目
@@ -184,8 +236,8 @@ $(function() {
 	
 	// 基盤教育基礎科目（社会科目）のセレクトのイベント設定
 	$society_select.change(function(){
-		console.log($(this).attr('name'));
-		console.log(parseInt($(this).val()));
+		//console.log($(this).attr('name'));
+		//console.log(parseInt($(this).val()));
 		credit_num.society_total = parseInt($(this).val()) * 2;
 	});
 });
